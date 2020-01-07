03d0de436ca8d1274b0a43ae814a2d9837ea1a06
Pavel Logachev <alhames@mail.ru>
2020-01-07 12:56:35 +0300

    Update to Symfony 5.0

--- DataCollector/DbDataCollector.php
+++ DataCollector/DbDataCollector.php
@@ -30,7 +30,7 @@ class DbDataCollector extends AbstractLogger implements DataCollectorInterface
     /**
      * {@inheritdoc}
      */
-    public function collect(Request $request, Response $response, \Exception $exception = null)
+    public function collect(Request $request, Response $response, \Throwable $exception = null)
     {
     }
 
